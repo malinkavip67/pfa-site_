6198a62a86d5bebaf0283e0d0ee0fd317cdfa8bf
malinkavip67 <297839929+malinkavip67@users.noreply.github.com>
2026-07-07 18:36:12 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Stats from "@/components/Stats";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Stats />
+    </>
+  );
+}
