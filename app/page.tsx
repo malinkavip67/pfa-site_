686b3bdf4854f5d3db3bd5d8d03228ff282941ba
malinkavip67 <297839929+malinkavip67@users.noreply.github.com>
2026-07-08 18:28:07 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Stats from "@/components/Stats";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <About />
       <Stats />
+      <Services />
     </>
   );
 }
